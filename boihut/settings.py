dd59f5c3b965e13ca90593911d83b5138860eb7d
Siam <mdshahbd2021@gmail.com>
2022-03-17 05:16:06 -0500

    staticfiles for heroku

--- boihut/settings.py
+++ boihut/settings.py
@@ -132,11 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[
-    STATIC_DIR,
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
