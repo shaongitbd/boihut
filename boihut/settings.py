064c23f663aefb53e7dd05a07ee2794cdec7df8b
Siam <mdshahbd2021@gmail.com>
2022-03-17 05:29:29 -0500

    requirements for heroku

--- boihut/settings.py
+++ boihut/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ow#nai80aww%+bfkq3@k%mgf0@2_hc^#qb8q4=)r0ngm!#50pg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
