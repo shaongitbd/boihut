cbd3273e6f5f16f1e4c4b16116cb0c72ebf7e364
Siam <mdshahbd2021@gmail.com>
2022-04-19 11:48:41 -0500

    html & css upgrade

--- boihut/settings.py
+++ boihut/settings.py
@@ -130,8 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+ os.path.join(BASE_DIR, 'static')
+ ]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -149,3 +152,4 @@ MESSAGE_TAGS = {
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
+
