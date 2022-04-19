cad37a1fa10432deeafd15897999a3931fe0659a
Siam <mdshahbd2021@gmail.com>
2022-04-19 06:50:16 -0500

    added input restrictions for views

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,29 @@ from django.contrib.auth.decorators import  login_required
 
 @login_required(login_url="/login")
 def checkout_req(request):
+
+    special_char_list = r"!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    email_special_char_list = r"!\"#$%&'()*+,/:;<=>?@[\]^`{|}~"
+
+    def num_checker(string):
+        return any(i.isdigit() for i in string)
+
+    def special_char_checker(string):
+        for i in string:
+            if i in special_char_list:
+                return True
+        return False
+
+    def email_special_char_checker(string):
+        if "@" in string:
+            email = string.split("@", "")
+            for i in email[0]:
+                if i in email_special_char_list:
+                    return True
+            return False
+        else:
+            return True
+
     if request.POST:
         req_user = request.user
 
@@ -83,8 +106,51 @@ def checkout_req(request):
             country = request.POST['country']
             order_note = request.POST['order_note']
 
+            # Chcking for any invalid/ suspecious inputs
 
+            # Checking for number
 
+            if num_checker(first_name) == True:
+                messages.error(request, "Sorry, First Name can't contain number")
+                return redirect("checkout_page")
+
+            if num_checker(last_name) == True:
+                messages.error(request, "Sorry, Last Name can't contain number")
+                return redirect("checkout_page")
+
+            if num_checker(division) == True:
+                messages.error(request, "Sorry, Division can't contain a number")
+                return redirect("checkout_page")
+
+            if num_checker(city) == True:
+                messages.error(request, "Sorry, City can't contain number")
+                return redirect("checkout_page")
+
+            if num_checker(country) == True:
+                messages.error(request, "Sorry, Country Name can't contain number")
+                return redirect("checkout_page")
+
+            # Checking for special character
+
+            if special_char_checker(first_name):
+                messages.error(request, "Sorry, First Name can't contain a special character.")
+                return redirect("checkout_page")
+
+            if special_char_checker(last_name):
+                messages.error(request, "Sorry, Last Name can't contain a special character.")
+                return redirect("checkout_page")
+
+            if special_char_checker(division):
+                messages.error(request, "Sorry, Division can't contain a special character.")
+                return redirect("checkout_page")
+
+            if email_special_char_checker(city):
+                messages.error(request, "Sorry, City  can't contain a special character.")
+                return redirect("checkout_page")
+
+            if email_special_char_checker(country):
+                messages.error(request, "Sorry, Country can't contain a special character.")
+                return redirect("checkout_page")
 
             save_invoice = invoice.objects.create(
                 order_id=order_save,
