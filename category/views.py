018022aa086b91f77b8cef8223f81e4b00182e45
Siam <mdshahbd2021@gmail.com>
2022-04-21 00:41:40 -0500

    added category name to category pages

--- category/views.py
+++ category/views.py
@@ -7,13 +7,14 @@ from category.models import Category
 
 
 def category(request, cat_slug=None):
-    cat_name = None
+    cat_name = ""
     if cat_slug is None:
         all_books = Paginator(Book.objects.all().order_by('-modified_on'),20)
     else:
         print(cat_slug)
         cat = Category.objects.get(slug=cat_slug)
         all_books = Paginator(Book.objects.all().filter(category=cat).order_by('-modified_on'),20)
+        cat_name= cat.category_name
 
     page = request.GET.get('page')
 
@@ -26,6 +27,7 @@ def category(request, cat_slug=None):
 
     context = {
         'books': books,
+         'category_name':cat_name,
     }
     return render(request,'books-cat.html', context)
 
